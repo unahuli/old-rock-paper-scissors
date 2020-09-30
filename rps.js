f83af981f0d12dcbf36cb37e2ef2403c2c671744
jian <gray.ratsky@gmail.com>
2020-09-30 23:32:13 +0800

    Created a separate file for the javascript code and a CSS file. Buttons added for player selection.

---
+++ rps.js
@@ -0,0 +1,61 @@
+function game() {
+    console.log((playRound(getPlayerSelection, computerPlay)));
+}
+
+function playRound(playerChoice, computerChoice) {
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
+
+    if(playerSelection === computerSelection) {
+        return `It's a draw! ${playerSelection} is equal to ${computerSelection}.`;
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors'
+            || playerSelection === 'Scissors' && computerSelection === 'Paper'
+            || playerSelection === 'Paper' && computerSelection === 'Rock'){
+        return `You Win! ${computerSelection} loses to ${playerSelection}.`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}.`
+    }
+}
+
+function getPlayerSelection() {
+    const playerSelection = prompt('Rock, Paper or Scissors?');
+    if (!playerSelection) {
+        alert('Make your choice.');
+    } else if (isValid(playerSelection)){
+        return toCapitalize(playerSelection);                
+    } else {
+        alert('Rock, Paper or Scissors only :(');
+    }
+}
+
+function computerPlay() {
+    const random = Math.floor(Math.random() * 3);
+    let computerSelection;
+    switch(random) {
+        case 0: 
+            computerSelection = 'Rock';
+            break;
+
+        case 1:
+            computerSelection = 'Paper';
+            break;
+
+        case 2:
+            computerSelection = 'Scissors';
+    }
+    return computerSelection;
+}
+
+function isValid(playerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    return playerSelection === 'rock' ||
+        playerSelection === 'paper' ||
+        playerSelection === 'scissors';
+}
+
+function toCapitalize(playerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    const firstLetter = playerSelection.slice(0,1);
+    playerSelection = playerSelection.replace(firstLetter, firstLetter.toUpperCase());
+    return playerSelection;
+}
